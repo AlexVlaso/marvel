9970f96256766d6f1c498aba9a17bdde3bee89d0
AlexVlaso <alexvlas.javaprog@gmail.com>
2023-05-04 13:37:33 +0300

    add CharacterService

--- src/services/CharacterService.js
+++ src/services/CharacterService.js
@@ -1,11 +1,29 @@
 class CharacterService {
-  baseUrl = "https://gateway.marvel.com:443/v1/public/";
-  apikey = "c609f1b9d796954a7d328f4c9130c80b";
+  _baseUrl = "https://gateway.marvel.com:443/v1/public/";
+  _apikey = "c609f1b9d796954a7d328f4c9130c80b";
+  async _getResponse(url) {
+    const response = await fetch(url);
+    return await response.json();
+  }
+  _transformData(char) {
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
+  }
+  async getCharacter(id) {
+    const serchUrl = `${this._baseUrl}characters/${id}?apikey=${this._apikey}`;
+    const res = await this._getResponse(serchUrl);
+    return this._transformData(res);
+  }
 
   async getAllCharacters() {
-    const serchUrl = `https://gateway.marvel.com:443/v1/public/characters?apikey=c609f1b9d796954a7d328f4c9130c80b`;
-    let data = await fetch(serchUrl);
-    return await data.json();
+    const serchUrl = `${this._baseUrl}characters?apikey=${this._apikey}`;
+    const res = await this._getResponse(serchUrl);
+    return res.map(this._transformData);
   }
 }
 
